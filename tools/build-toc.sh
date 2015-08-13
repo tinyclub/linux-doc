a5e9090f0611f973b867cc5b487033157c1167bd
Wu Zhangjin <wuzhangjin@gmail.com>
2015-08-13 09:41:52 +0800

    tools: Fix up build-toc.sh
    Allow to detect languages automatically.
    
    Signed-off-by: Wu Zhangjin <wuzhangjin@gmail.com>

--- tools/build-toc.sh
+++ tools/build-toc.sh
@@ -8,6 +8,23 @@
 md=$1
 [ ! -f "$md" ] && exit "ERROR: No such file" && exit 1
 
+# Detect target language (only support en/ and zh-cn/ currently)
+contents="Contents"
+echo ${md} | grep -q "zh-cn"
+if [ $? -eq 1 ]; then
+	echo $PWD | grep -q "zh-cn"
+	[ $? -eq 0 ] && contents="目录"
+else
+	contents="目录"
+fi
+
+# Drop old toc
+start_line=`grep -n -m1 "^## ${contents}" $md | cut -d':' -f1`
+end_line=`grep -n -m1 "^# " $md | cut -d':' -f1`
+((end_line--))
+sed -i -e "/<span /d" ${md}
+sed -i -e "${start_line},${end_line}d" ${md}
+
 # Generate a random toc id to avoid conflict
 toc="toc_${RANDOM}_${RANDOM}_"
 
@@ -21,7 +38,7 @@ sed -i -e "${ins_line}i\\\\" ${md}
 
 echo > toc.tmp
 
-grep "^###* " -ur $md | grep -v "## Contents" | grep -n "^#" | \
+grep "^###* " -ur $md | grep -v "## ${contents}" | grep -n "^#" | \
 	sed -e "s/^\([0-9]*\):/\1a/g;" |\
 	sed -e "s/\([0-9]*\)a\(#[^ ]*\) \(.*\)/\1a\2 [\3](#$toc\1)/g" |\
 	sed -e "s/#####/+            -   /g;s/####/+        -   /g" |\
@@ -36,12 +53,12 @@ sed -i -e "s/^+   /   /g;" toc.tmp
 # Insert the toc
 
 sed -i -e "${ins_line}r toc.tmp" ${md}
-sed -i -e "${ins_line}a## Contents\\" ${md}
+sed -i -e "${ins_line}a## ${contents}\\" ${md}
 sed -i -e "${ins_line}a\\\\" ${md}
 
 # Replace the #* with h* + id info
 t=0
-for line in `grep -n "^##" $md | grep -v "## Contents" | cut -d':' -f1`
+for line in `grep -n "^##" $md | grep -v "## ${contents}" | cut -d':' -f1`
 do
 	((line+=t))
 	((t++))
